7aec457881b5aa5cf2b616e8db93450afb9a76f0
Ashutosh <ashutoshmalik99@gmail.com>
2025-09-07 23:23:08 +0530

    new file added

--- Payment_Service/src/StringReverse.java
+++ Payment_Service/src/StringReverse.java
@@ -1,3 +1,9 @@
 public class StringReverse {
-    
+    public static String reverse(String str){
+        String reverse="";
+        for (int i = str.length()-1; i >=0 ; i--) {
+              reverse=reverse+str.charAt(i);
+        }
+        return reverse;
+    }
 }
